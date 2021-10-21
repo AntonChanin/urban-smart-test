1caae1fd62629f02a9ca92b0b03f5bda54a9eb64
antonvh <antonhaninvl@gmail.com>
2021-10-21 15:39:27 +0300

    routing lock for unauthorized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Login from './components/login/Login';
 import Profile from './components/profile/Profile';
+import { selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route path="/profile" component={Profile} />
+          {user && <Route path="/profile" component={Profile} />}
           <Redirect to="/" />
         </Switch>
       </div>
